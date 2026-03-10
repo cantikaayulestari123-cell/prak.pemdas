46d6bdc9bfbfa5750939c10a7f31d3a59e855cda
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-03-10 19:46:49 +0700

    menampilkan status kelulusan

--- file2.cpp
+++ file2.cpp
@@ -24,4 +24,15 @@ int main(){
     cin >> nilBI;
     cout <<"Masukkan nilai Matematika = ";
     cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/;
+    //IF (rerata >= 60)
+    //    status = "Lolos";
+    //else
+      //  status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+    
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan  nilai rerata = "<<rerata<<endl;
+    
 }
